78dd96aaa72b6bef74dc6840db4a333b6dea5a4a
tom_villalba <totovillalba.tv@gmail.com>
2023-01-24 19:22:00 -0300

    Proyecto actualizado a TypeScript

--- src/components/notes/NoteScreen.tsx
+++ src/components/notes/NoteScreen.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NotesAppBar } from './NotesAppBar';
 
-
 export const NoteScreen = () => {
+
 	return (
 		<div className="notes__main-content">
 			<NotesAppBar />
-			<div className="notes__content">
 
+			<div className="notes__content">
 				<input
 					type="text"
 					placeholder="Some awesome title"
